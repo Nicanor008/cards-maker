6d630b7775d9270085b8acadc247350c809e2edb
Nicanor008 <nicanorkorir008@gmail.com>
2020-02-28 17:03:42 +0300

    Added description

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./homePage.css";
 
 import LoveIcon from "../../images/Love2Icons.svg";
+import Wedding from "../../images/Wedding.svg";
 
 const HomePage = () => {
   return (
@@ -9,34 +10,52 @@ const HomePage = () => {
       {/* image */}
       <div className="bodyWrapper love2Icons">
         <figure className="image container is-centered">
-            <img src={LoveIcon} alt="Love Icons" className="image is-centered" />
+          <img src={LoveIcon} alt="Love Icons" className="image is-centered" />
         </figure>
       </div>
 
       {/* about cardsmaker description */}
-      <div className="container">
-        <div class="columns is-mobile">
-          <div class="column">
-            <p class="bd-notification is-primary">First column</p>
-            <p>
-              sdfds fsdfsdfsdfsdf sdf sdfsdf sdf dsfsd f sd f ds fsdfsdfdsf
-              sdfsdfsdfsdf sdf sdfdsfsdfsdfsdf sdfsd
+      <div className="container columnDescriptionWrapper">
+        <div className="columns is-mobile">
+          <div className="column">
+            <p className="bd-notification is-primary rowHeader">Create</p>
+            <p className="rowTextContent">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when
             </p>
           </div>
-          <div class="column">
-            <p class="bd-notification is-primary">First column</p>
-            <p>
-              sdfds fsdfsdfsdfsdf sdf sdfsdf sdf dsfsd f sd f ds fsdfsdfdsf
-              sdfsdfsdfsdf sdf sdfdsfsdfsdfsdf sdfsd
+          <div className="column">
+            <p className="bd-notification is-primary rowHeader">Collaborate</p>
+            <p className="rowTextContent">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when
             </p>
           </div>
-          <div class="column">
-            <p class="bd-notification is-primary">First column</p>
-            <p>
-              sdfds fsdfsdfsdfsdf sdf sdfsdf sdf dsfsd f sd f ds fsdfsdfdsf
-              sdfsdfsdfsdf sdf sdfdsfsdfsdfsdf sdfsd
+          <div className="column">
+            <p className="bd-notification is-primary rowHeader">Share</p>
+            <p className="rowTextContent">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when
             </p>
           </div>
+        </div>
+      </div>
+
+      <div className="container columns is-mobile">
+        <div className="column">
+          <figure className="image is-3by1">
+            <img src={Wedding} alt="Wedding people" />
+          </figure>
+        </div>
+        <div className="column">
+          <p className="rowTextContent rowDescriptionText">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when<br /><br />
+          <button className="button is-danger">Create Cards</button></p>
         </div>
       </div>
     </>
